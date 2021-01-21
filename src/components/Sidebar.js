544602a2f6e87a0d849029bc3e7c5ff3cf1f5ac4
BingShawLim <shaw.a.muvee@gmail.com>
2021-01-21 08:10:01 -0600

    initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Links from '../constants/links'
+import Categories from '../components/Categories'
+import { IoMdClose } from 'react-icons/io'
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <aside className={`sidebar ${isOpen === true ? 'showSidebar' : ''}`}>
+      <button onClick={toggle} className="close-btn">
+        <IoMdClose />
+      </button>
+      <div className="sidebar-container">
+        <Links styleClass="sidebar-links" />
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
